e23e0cb42ecdfa3d7afefe093cc642ad6ac72bc2
jasonin <jasonin@hotmail.com>
2018-08-27 20:02:24 +0800

    Add landing page

---
+++ src/components/HomePageContainer/HomePageContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import SearchBar from '../SearchBar/SearchBar'
+
+class HomePageContainer extends Component {
+
+  render() {
+    return (
+      <div className="container HomePageContainer"> 
+        <SearchBar />
+      </div>
+    )
+  }
+}
+export default (HomePageContainer)
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import UserProfileContainer from './components/UserProfileContainer/UserProfileContainer'
 import SearchResultsContainer from './components/SearchResultsContainer/SearchResultsContainer';
+import HomePageContainer from './components/HomePageContainer/HomePageContainer';
 
 export default () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path='/' component={ HomePageContainer }/>
         <Route path='/search/:query' component={ SearchResultsContainer }/>
         <Route path='/user/:login' component={ UserProfileContainer }/>
       </Switch>
